3ce15c25bf9ef190e89a2c84d704a07b2597d2f3
Anuj Som <128492211+anuj9084@users.noreply.github.com>
2025-08-15 19:45:30 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,14 +1,12 @@
 terraform {
-  required_version = ">= 1.0.0"
-
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      source = "hashicorp/azurerm"
+      version = "4.40.0"
     }
   }
 }
 
 provider "azurerm" {
-  features {}
+  # Configuration options
 }
