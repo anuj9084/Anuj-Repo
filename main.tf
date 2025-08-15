151afa1e3d5b98f14399217cdae6de866083f08c
Anuj Som <128492211+anuj9084@users.noreply.github.com>
2025-08-15 19:46:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,19 +6,10 @@ resource "azurerm_resource_group" "rg" {
 
 # Storage Account
 resource "azurerm_storage_account" "storage" {
-  name                     = "anujstorage${random_integer.suffix.result}"
+  name                     = "anujstorage"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  tags = {
-    environment = "demo"
-  }
-}
-
-# Random suffix for unique storage name
-resource "random_integer" "suffix" {
-  min = 1000
-  max = 9999
 }
